1ce075e59e4dc36f01de1c4c7c39608f7365e3ef
jaikumar <tjjaikumar@gmail.com>
2020-01-17 21:18:15 +0530

    constant file used for numbers

--- src/App.js
+++ src/App.js
@@ -1,68 +1,68 @@
 import React, { useState, useEffect } from "react";
 import ScoreBoard from "./Components/ScoreBoard";
 import KeyBoard from "./Components/KeyBoard";
+import { MAX_ROLLS, GENERAL_ROLL_LIMIT, MAX_ROLLS_PER_FRAME, MAX_SCORE } from "./Constants/constant";
 
 function App() {
   const [score, setScore] = useState([]);
   const [rolls, setRolls] = useState([]);
 
   const isNextFrameAvailble = () => {
-    if (rolls.length === 21) return false;
-    if (rolls.length === 20 && rolls[18] + rolls[19] < 10) {
+    if (rolls.length === MAX_ROLLS) return false;
+    if (rolls.length === (MAX_ROLLS - 1) && rolls[GENERAL_ROLL_LIMIT - 1] + rolls[GENERAL_ROLL_LIMIT] < MAX_SCORE) {
       return false;
     }
     return true;
   };
 
-  const isSecondRoll = () => rolls.length % 2 === 1 && rolls.length < 19;
-  const isLastRoll = () => rolls.length === 19 && rolls[18] !== 10;
-  const isLastBonuspresent = () =>
-    rolls.length === 20 && rolls[18] == 10 && rolls[19] !== 10;
+  const isStrike = (roll) => (roll === MAX_SCORE)
+  const isFirstRoll = () => (rolls.length % MAX_ROLLS_PER_FRAME === 0)
+  const isSecondRoll = () => (!isFirstRoll() && rolls.length < GENERAL_ROLL_LIMIT);
+  const isLastRoll = () => (rolls.length === GENERAL_ROLL_LIMIT && rolls[GENERAL_ROLL_LIMIT - 1] !== MAX_SCORE)
+  const isLastBonuspresent = () => (rolls.length === (MAX_ROLLS - 1) && isStrike(rolls[GENERAL_ROLL_LIMIT - 1]) && rolls[GENERAL_ROLL_LIMIT] !== MAX_SCORE)
+
 
   const isRollValid = roll => {
     if (isSecondRoll() || isLastRoll() || isLastBonuspresent())
-      return roll <= 10 - rolls[rolls.length - 1];
+      return roll <= MAX_SCORE - rolls[rolls.length - 1];
     return true;
   };
 
   useEffect(() => {
     //set null for secondary roll after strike
-    if (rolls.length % 2 === 1 && rolls[rolls.length - 1] === 10) {
-      console.error("NULLED");
+    if (!isFirstRoll() && isStrike(rolls[rolls.length - 1])) {
       setRolls([...rolls, null]);
     }
   }, [rolls]);
 
   useEffect(() => {
     let scores = [];
-    var totalScore = 0;
-    for (var index = 0; index < rolls.length; index += 2) {
-      if (index <= 18) {
-        let rollsPerFrame = rolls.slice(index, index + 2);
+    var sum = 0;
+    for (var index = 0; index < rolls.length; index += MAX_ROLLS_PER_FRAME) {
+      if (index <= (GENERAL_ROLL_LIMIT - 1)) {
+        let rollsPerFrame = rolls.slice(index, index + MAX_ROLLS_PER_FRAME);
         let bonus = rolls.slice(index + 2, index + 5);
-        totalScore += getScorePerFrame(rollsPerFrame, bonus);
-        // if (rollsPerFrame[1]){
-        scores.push(totalScore);
-        // }
+        sum += getScorePerFrame(rollsPerFrame, bonus);
+        scores.push(sum);
       }
     }
-    if (rolls.length > 18) {
-      totalScore += rolls.slice(18, 21).reduce((a, b) => a + b);
-      scores.push(totalScore);
+    if (rolls.length > (GENERAL_ROLL_LIMIT - 1)) {
+      sum += rolls.slice(GENERAL_ROLL_LIMIT - 1, MAX_ROLLS).reduce((a, b) => a + b);
+      scores.push(sum);
     }
     setScore(scores);
   }, [rolls]);
 
   const getScorePerFrame = (rolls, bonus) => {
-    if (rolls[0] + rolls[1] < 10) {
+    if (rolls[0] + rolls[1] < MAX_SCORE) {
       //general score
       return rolls[0] + (rolls[1] || 0);
     }
-    if (rolls[0] === 10 && bonus[1] === null) {
+    if (isStrike(rolls[0]) && bonus[1] === null) {
       //2 consequtive strikes
       return rolls[0] + bonus[0] + (bonus[2] || 0);
     }
-    if (rolls[0] === 10 || rolls[1] === 10) {
+    if (isStrike(rolls[0]) || isStrike(rolls[1])) {
       //single strike
       return (rolls[0] || rolls[1]) + (bonus[0] || 0) + (bonus[1] || 0);
     }
@@ -78,16 +78,15 @@ function App() {
 
   const getTransformedData = data => {
     let result = [];
-    for (let i = 0; i < data.length; i += 2) {
-      // console.log('index',i)
+    for (let i = 0; i < data.length; i += MAX_ROLLS_PER_FRAME) {
       result.push({
         primary: data[i],
         secondary: data[i + 1]
       });
     }
-    // console.log('result',result,data.length)
     return result;
   };
+
   return (
     <React.Fragment>
       <ScoreBoard rolls={getTransformedData(rolls)} total={score} />
